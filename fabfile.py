aeb43f9c8f9cbe99adfb4284cfcd29a4f68b0c9c
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-09-23 03:40:22 -0400

    Add Fabric Command to Create Adam's Staging Site

--- fabfile.py
+++ fabfile.py
@@ -17,6 +17,17 @@ def make_staging():
         sudo('bash /root/bin/make_staging.sh')
 
 
+def make_adam_staging():
+    warning_text = '''
+    This is a destructive action that will clear out any modifications to
+    current staging files & database.
+
+    Are you sure you wish to continue?
+    '''
+    if confirm(warning_text):
+        sudo('/root/bin/make_adam_staging.sh', shell=False)
+
+
 def backup_wordpress():
     sudo('bash /root/bin/backup_wordpress.sh manual')
 
